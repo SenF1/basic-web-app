442f170d365c436c30f5d53a7d5462b75604922d
Sen Feng <senfeng6@gmail.com>
2023-09-19 15:55:32 -0400

    Added query for addition

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -33,4 +33,9 @@ describe("QueryProcessor", () => {
             "I dont know your name."
           ));
     });
+    
+    test('should add numbers', () => {
+        expect(QueryProcessor("What is 5 plus 52?")).toEqual("57");
+        expect(QueryProcessor("What is 12 plus 1?")).toEqual("13");
+    });
 });
